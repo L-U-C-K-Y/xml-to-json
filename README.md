3ff9776f2ed3cd00ec94cc39d1a5cdcc8deeb441
Lucky Bassi <lucky@jobdone.app>
2022-01-19 17:19:16 +0100

    feat(app): add initial code

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # xml-to-json
+
+Simple script to convert `.xml` files to `.json` files.
+
+## Getting Started
+
+1. Place `.xml` files in the `xml` folder.
+2. Install and run the project with `yarn` and `yarn run start`
+3. Converted `.json` files appear in the `json` folder
+4. Profit 💸
---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import xml2js from 'xml2js';
+import fs from 'fs';
+import fg from 'fast-glob';
+
+const start = async () => {
+    const filepaths = await fg(['xml/*.xml']);
+
+    filepaths.forEach((filepath) => {
+        const xmlParser = new xml2js.Parser();
+        const updatedFilepath = 'json' + filepath.substring(3, filepath.length - 4) + '.json';
+
+        fs.readFile(filepath, (err: any, data: any) => {
+            xmlParser.parseString(data, (err: any, result: any) => {
+                fs.writeFile(updatedFilepath, JSON.stringify(result, null, 2), (err) => {
+                    if (err) {
+                        console.error(err);
+                    } else {
+                        console.info(filepath + ' successfully converted to ' + updatedFilepath);
+                    }
+                });
+            });
+        });
+    });
+};
+
+start();
